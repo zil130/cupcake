f52573182d7a6501d49368ef7e5c3949117c6146
zil130 <55101149+zil130@users.noreply.github.com>
2024-05-05 14:18:25 -0300

    init: start project, webpack install

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[contenthash].js',
+      clean: true
+    }
+  }
+}
