82489519116143c513143bceb63d4ab12d50eeff
zil130 <55101149+zil130@users.noreply.github.com>
2024-05-05 15:26:03 -0300

    build: ts install

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = (env) => {
   return {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash].js',
@@ -15,5 +15,17 @@ module.exports = (env) => {
         template: path.resolve(__dirname, 'public', 'index.html'),
       }),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
   }
 }
